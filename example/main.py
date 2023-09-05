0669b46d990ddf21ab317b59b0a0401dea222d9c
ShashiKant <shashikant@paralleldots.com>
2023-09-05 22:36:35 +0530

    1) Some more bug fixes 2) Added examples

--- example/main.py
+++ example/main.py
@@ -1,8 +1,41 @@
-from src import FastPy, Headers, Cookies
+from src import FastPy, Headers, Cookies, HttpException
+from pydantic import BaseModel
+
+class StudentIn(BaseModel):
+    name: str
+    roll: int
+
+class Student(BaseModel):
+    id: int
+    name: str
+    roll: int
 
 app = FastPy()
 
-@app.get("/get/{id}")
-async def get_id(name: str, id: int) -> int:
-    print(name, id, "get called.")
-    return id
+CNT_ID = 0
+STUDENTS = {}
+
+@app.post("/createStudent")
+async def create_student(student: StudentIn) -> Student:
+    global CNT_ID
+    global STUDENTS
+    CNT_ID += 1
+    id = CNT_ID
+    STUDENTS[id] = student.model_dump()
+    STUDENTS[id]["id"] = id
+    return STUDENTS[id]
+
+@app.get("/getStudent/{id}")
+async def get_student(id: int) -> Student:
+    if id in STUDENTS:
+        return STUDENTS[id]
+    else:
+        raise HttpException(404, f"Student not found for id {id}.")
+    
+@app.get("/verifyHeaders")
+async def verify_headers(authentication: str = Headers()) -> str:
+    return authentication
+
+@app.get("/verifyCookies")
+async def verify_cookies(name: str = Cookies(), roll: str = Cookies()):
+    return {"name": name, "roll": roll}
